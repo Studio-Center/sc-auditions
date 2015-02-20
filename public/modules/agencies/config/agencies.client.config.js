5dd71472dded6b34aacb7d88a8e3faca72214213
rob <rob@studiocenter.com>
2015-02-20 17:17:01 -0500

    hide agencies

--- public/modules/agencies/config/agencies.client.config.js
+++ public/modules/agencies/config/agencies.client.config.js
@@ -5,9 +5,9 @@ angular.module('agencies').run(['Menus', 'Authentication',
 	function(Menus, Authentication) {
 		// Set top bar menu items
 		if(Authentication.user.role === 'admin' || Authentication.user.role === 'producer/auditions director'){
-			Menus.addMenuItem('topbar', 'Agencies', 'agencies', 'dropdown', '/agencies(/create)?');
-			Menus.addSubMenuItem('topbar', 'agencies', 'List Agencies', 'agencies');
-			Menus.addSubMenuItem('topbar', 'agencies', 'New Agency', 'agencies/create');
+			//Menus.addMenuItem('topbar', 'Agencies', 'agencies', 'dropdown', '/agencies(/create)?');
+			//Menus.addSubMenuItem('topbar', 'agencies', 'List Agencies', 'agencies');
+			//Menus.addSubMenuItem('topbar', 'agencies', 'New Agency', 'agencies/create');
 		}
 	}
 ]);
