36d624eee402edb8fcc443333e5180688a9197b4
rob <rob@studiocenter.com>
2015-03-10 15:50:38 -0400

    more semantics, date/time options

--- public/modules/talents/config/talents.client.config.js
+++ public/modules/talents/config/talents.client.config.js
@@ -4,8 +4,8 @@
 angular.module('talents').run(['Menus', 'Authentication',
 	function(Menus, Authentication) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Talents', 'talents', 'dropdown', '/talents(/create)?', false, ['admin','producer/auditions director','talent director']);
-		Menus.addSubMenuItem('topbar', 'talents', 'List Talents', 'talents', false, ['admin','producer/auditions director','talent director']);
+		Menus.addMenuItem('topbar', 'Talent', 'talents', 'dropdown', '/talents(/create)?', false, ['admin','producer/auditions director','talent director']);
+		Menus.addSubMenuItem('topbar', 'talents', 'List Talent', 'talents', false, ['admin','producer/auditions director','talent director']);
 		Menus.addSubMenuItem('topbar', 'talents', 'New Talent', 'talents/create', false, ['admin','producer/auditions director','talent director']);
 	}
 ]);
