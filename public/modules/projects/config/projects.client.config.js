39555b0120193a154ee2ac68f76eb4b68891650d
rob <rob@studiocenter.com>
2015-03-13 09:51:59 -0400

    various
    rename auditions
    publish auditions
    new production coordinator permissions

--- public/modules/projects/config/projects.client.config.js
+++ public/modules/projects/config/projects.client.config.js
@@ -6,7 +6,7 @@ angular.module('projects').run(['Menus', 'Authentication',
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Projects', 'projects', 'dropdown', '/projects(/create)?', false);
 		Menus.addSubMenuItem('topbar', 'projects', 'List Projects', 'projects', false);
-		Menus.addSubMenuItem('topbar', 'projects', 'New Project', 'projects/create', false, false, ['admin','producer/auditions director']);
+		Menus.addSubMenuItem('topbar', 'projects', 'New Project', 'projects/create', false, false, ['admin','producer/auditions director', 'production coordinator']);
 		Menus.addSubMenuItem('topbar', 'projects', 'Start A New Audition Project', 'projects/new-audition-form', false);
 	}
 ]);
