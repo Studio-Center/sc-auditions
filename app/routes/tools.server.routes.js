d1735ada16b6e424611d1a0c8eac14bf60aae93b
rob <rob@studiocenter.com>
2015-03-25 12:39:27 -0400

    various
    New tools menu
    various visual layout changes

---
+++ app/routes/tools.server.routes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(app) {
+	var users = require('../../app/controllers/users');
+	var tools = require('../../app/controllers/tools');
+
+	// Tools Routes
+	app.route('/tools')
+		.get(tools.list)
+		.post(users.requiresLogin, tools.create);
+
+	app.route('/tools/:toolId')
+		.get(tools.read)
+		.put(users.requiresLogin, tools.hasAuthorization, tools.update)
+		.delete(users.requiresLogin, tools.hasAuthorization, tools.delete);
+
+	// Finish by binding the Tool middleware
+	app.param('toolId', tools.toolByID);
+};
