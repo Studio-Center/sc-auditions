b801178a478f55aa15dbe303faabf31dfdaf304e
rob <rob@studiocenter.com>
2015-03-30 10:04:22 -0400

    sounders

--- public/modules/projects/controllers/projects.client.controller.js
+++ public/modules/projects/controllers/projects.client.controller.js
@@ -18,6 +18,7 @@ angular.module('projects').controller('ProjectsController', ['$scope', '$statePa
 		$scope.statusOpts = ['In Progress', 'On Hold', 'Pending Client Decision', 'Booked', 'Canceled', 'ReAuditioned', 'Dead'];
 		$scope.priorityOpts = ['None', 'Very low', 'Low', 'Medium', 'High', 'Very high'];
 		$scope.phaseStatusOpts = ['in progress','open','complete','suspended'];
+		$scope.soundersOpts = ['Sounders', 'No Sounders'];
 		$scope.talentStatus = ['Cast', 'Emailed', 'Scheduled', 'Message left', 'Out', 'Received needs to be posted', 'Posted', 'Not Posted (Bad Read)'];
 		$scope.loadAudio = 0;
 		$scope.audio = [];
