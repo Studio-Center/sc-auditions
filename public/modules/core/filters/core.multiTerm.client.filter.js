c3b1052c446e5216378b283b762beb8c856d0e91
rob <rob@studiocenter.com>
2015-06-02 12:44:54 -0400

    union multi-filter tweak

--- public/modules/core/filters/core.multiTerm.client.filter.js
+++ public/modules/core/filters/core.multiTerm.client.filter.js
@@ -1,20 +1,12 @@
 'use strict';
 angular.module('core').filter('multiTerm', function($filter){
-     return function (input, searchText, AND_OR) {
-        if(typeof searchText !== 'undefined'){
-            var returnArray = [];
-            var wordArray = searchText ? searchText.toLowerCase().split(/\s+/) : [];
-            var wordCount = wordArray.length;
-            for(var i=0;i<wordCount;i++){
-                returnArray = $filter('filter')(input, wordArray[i]);
-            }
-            if(returnArray.length > 0){
-                return returnArray;
-            } else {
-                return input;
-            } 
-        } else {
-            return input;
+     return function(inputArray, searchText){
+        var wordArray = searchText ? searchText.toLowerCase().split(/\s+/) : [];
+        var wordCount = wordArray.length;
+        for(var i=0;i<wordCount;i++){
+            inputArray = $filter('filter')(inputArray, wordArray[i], true);
+            console.log(wordArray[i]);
         }
-    };
+        return inputArray;
+    }
 });
