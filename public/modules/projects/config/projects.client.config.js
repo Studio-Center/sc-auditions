4e89804c3e89ae257659734445b9a62329acfebe
rob <rob@studiocenter.com>
2015-06-05 14:14:07 -0400

    project admin trophy display

--- public/modules/projects/config/projects.client.config.js
+++ public/modules/projects/config/projects.client.config.js
@@ -5,7 +5,7 @@ angular.module('projects').run(['Menus', 'Authentication',
 	function(Menus, Authentication) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Projects', 'projects', 'dropdown', '/projects(/create)?', false);
-		Menus.addSubMenuItem('topbar', 'projects', 'Projects Client', 'projects-client', false);
+		Menus.addSubMenuItem('topbar', 'projects', 'Client Portal', 'projects-client', false);
 		Menus.addSubMenuItem('topbar', 'projects', 'List Projects', 'projects', false);
 		Menus.addSubMenuItem('topbar', 'projects', 'New Project', 'projects/create', false, false, ['admin','producer/auditions director', 'production coordinator']);
 		Menus.addSubMenuItem('topbar', 'projects', 'Start A New Audition Project', 'projects/new-audition-form', false, false, ['client']);
