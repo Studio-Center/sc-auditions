64360d3332d6a8ac0ed5a707faeb2c0a41e124b4
rob <rob@studiocenter.com>
2015-06-08 15:10:08 -0400

    menu structure update

--- public/modules/reports/config/reports.client.config.js
+++ public/modules/reports/config/reports.client.config.js
@@ -5,7 +5,8 @@ angular.module('reports').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Reports', 'reports', 'dropdown', '/reports(/create)?', false, ['admin','producer/auditions director','talent director']);
-		Menus.addSubMenuItem('topbar', 'reports', 'List Reports', 'reports');
-		Menus.addSubMenuItem('topbar', 'reports', 'New Report', 'reports/create');
+		Menus.addSubMenuItem('topbar', 'reports', 'Generate Reports', 'reports');
+		Menus.addSubMenuItem('topbar', 'reports', 'Missing Auditions', 'reports/missing-auditions');
+		Menus.addSubMenuItem('topbar', 'reports', 'Auditions Booked', 'reports/auditions-booked');
 	}
 ]);
--- public/modules/tools/config/tools.client.config.js
+++ public/modules/tools/config/tools.client.config.js
@@ -5,7 +5,9 @@ angular.module('tools').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Tools', 'tools', 'dropdown', '/tools(/create)?', false, ['admin','producer/auditions director', 'production coordinator']);
-		Menus.addSubMenuItem('topbar', 'tools', 'List Tools', 'tools');
-		Menus.addSubMenuItem('topbar', 'tools', 'New Tool', 'tools/create');
+		Menus.addSubMenuItem('topbar', 'tools', 'Call List', 'tools/call-list');
+		Menus.addSubMenuItem('topbar', 'tools', 'Email Talent', 'tools/email-talent');
+		Menus.addSubMenuItem('topbar', 'tools', 'Backup/Restore', 'tools/backup-restore');
+		Menus.addSubMenuItem('topbar', 'tools', 'Delete Projects', 'tools/delete-projects');
 	}
 ]);
