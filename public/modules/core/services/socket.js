1d807642a4aea067bf18cb1439fd61c6f56ce1a0
rob <rob@studiocenter.com>
2015-06-10 17:02:15 -0400

    socket.io dynamic host + port

--- public/modules/core/services/socket.js
+++ public/modules/core/services/socket.js
@@ -5,7 +5,7 @@ angular.module('core').factory('Socket', ['socketFactory',
     function(socketFactory) {
         return socketFactory({
             prefix: '',
-            ioSocket: io.connect('http://localhost:3000')
+            ioSocket: io.connect(location.host)
         });
     }
 ]);
