609d53a795a4abe93f9c3771ade7d43a58ecbff5
rob <rob@studiocenter.com>
2015-06-29 14:11:02 -0400

    code cleanup, socketio list updates

--- app/controllers/reports.server.controller.js
+++ app/controllers/reports.server.controller.js
@@ -27,14 +27,6 @@ var mongoose = require('mongoose'),
 // methods for missing auditions report
 exports.findMissingAuds = function(req, res){
 	var callTalents = {}, talentId, missingCnt = 0;
-	// var searchCriteria = {'talent': { 
-	// 								$not: {
-	// 									$elemMatch: { 
-	// 										'status': ['Out', 'Posted', 'Not Posted (Bad Read)']
-	// 									}
-	// 								} 
-	// 							}
-	// 					};
 
 	var yesterday = new Date(req.body.dateFilter);
 	yesterday.setDate(yesterday.getDate() - 1);
@@ -124,7 +116,7 @@ exports.findMissingAuds = function(req, res){
 						message: errorHandler.getErrorMessage(err)
 					});
 				} else {
-					res.jsonp({count: missingCnt, results:callTalents});
+					return res.jsonp({count: missingCnt, results:callTalents});
 				}
            	});
 			
@@ -383,92 +375,6 @@ var yesterday = new Date(req.body.dateFilterStart);
 			}
 
 		});
-	});
-};
-
-/**
- * Create a Report
- */
-exports.create = function(req, res) {
-	var report = new Report(req.body);
-	report.user = req.user;
-
-	report.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(report);
-		}
-	});
-};
-
-/**
- * Show the current Report
- */
-exports.read = function(req, res) {
-	res.jsonp(req.report);
-};
-
-/**
- * Update a Report
- */
-exports.update = function(req, res) {
-	var report = req.report ;
-
-	report = _.extend(report , req.body);
-
-	report.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(report);
-		}
-	});
-};
-
-/**
- * Delete an Report
- */
-exports.delete = function(req, res) {
-	var report = req.report ;
-
-	report.remove(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(report);
-		}
-	});
-};
-
-/**
- * List of Reports
- */
-exports.list = function(req, res) { Report.find().sort('-created').populate('user', 'displayName').exec(function(err, reports) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(reports);
-		}
-	});
-};
-
-/**
- * Report middleware
- */
-exports.reportByID = function(req, res, next, id) { Report.findById(id).populate('user', 'displayName').exec(function(err, report) {
-		if (err) return next(err);
-		if (! report) return next(new Error('Failed to load Report ' + id));
-		req.report = report ;
-		next();
 	});
 };
 
