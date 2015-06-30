d19e72e9a8d74ce0cb0b983b5578e23127698d06
rob <rob@studiocenter.com>
2015-06-30 12:37:48 -0400

    enable social media logins

--- config/env/development.js
+++ config/env/development.js
@@ -6,24 +6,24 @@ module.exports = {
 		title: 'Studio Center Auditions - Development Environment'
 	},
 	facebook: {
-		clientID: process.env.FACEBOOK_ID || 'APP_ID',
-		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-		callbackURL: 'http://localhost:3000/auth/facebook/callback'
+		clientID: process.env.FACEBOOK_ID || '848445448582782',
+		clientSecret: process.env.FACEBOOK_SECRET || '9f937db4218b73809eb52ec3c43e7e12',
+		callbackURL: 'http://dev.studiocenterauditions.com/auth/facebook/callback'
 	},
 	twitter: {
-		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
-		callbackURL: 'http://localhost:3000/auth/twitter/callback'
+		clientID: process.env.TWITTER_KEY || 'MJAdQiMJz13Qd5OzNo9RMVqhc',
+		clientSecret: process.env.TWITTER_SECRET || 'gJRB8srQ2ipQ7aWSW120tM5XJ2OvHRfR4NhRppCzd9IDfJXiuk',
+		callbackURL: 'http://dev.studiocenterauditions.com/auth/twitter/callback'
 	},
 	google: {
-		clientID: process.env.GOOGLE_ID || 'APP_ID',
-		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
-		callbackURL: 'http://localhost:3000/auth/google/callback'
+		clientID: process.env.GOOGLE_ID || '1037177876522.apps.googleusercontent.com',
+		clientSecret: process.env.GOOGLE_SECRET || 'HhBkixnP1j8bFyXJ2l8srmiJ',
+		callbackURL: 'http://dev.studiocenterauditions.com/auth/google/callback'
 	},
 	linkedin: {
-		clientID: process.env.LINKEDIN_ID || 'APP_ID',
-		clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
-		callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+		clientID: process.env.LINKEDIN_ID || '77fpjkn9n5c8ag',
+		clientSecret: process.env.LINKEDIN_SECRET || 'PZElXXIipnGRY4rP',
+		callbackURL: 'http://dev.studiocenterauditions.com/auth/linkedin/callback'
 	},
 	mailer: {
 		from: process.env.MAILER_FROM || 'no-reply@studiocenterauditions.com',
