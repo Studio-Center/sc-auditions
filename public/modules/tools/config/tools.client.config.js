8707c4e15bae59ce6be921af8d6e81ad57d1aa02
rob <rob@studiocenter.com>
2015-07-09 11:09:48 -0400

    menu sort order adjust

--- public/modules/tools/config/tools.client.config.js
+++ public/modules/tools/config/tools.client.config.js
@@ -4,7 +4,7 @@
 angular.module('tools').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Tools', 'tools', 'dropdown', '/tools(/create)?', false, ['admin','producer/auditions director', 'production coordinator']);
+		Menus.addMenuItem('topbar', 'Tools', 'tools', 'dropdown', '/tools(/create)?', false, ['admin','producer/auditions director', 'production coordinator'], 1);
 		Menus.addSubMenuItem('topbar', 'tools', 'Call List', 'tools/call-list');
 		Menus.addSubMenuItem('topbar', 'tools', 'Email Talent', 'tools/email-talent');
 		Menus.addSubMenuItem('topbar', 'tools', 'Backup/Restore', 'tools/backup-restore');
