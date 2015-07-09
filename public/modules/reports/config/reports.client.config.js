a760ba5fd4076358d31a57b1a36608e22491bdbd
rob <rob@studiocenter.com>
2015-07-09 15:26:08 -0400

    grunt tweaks

--- public/modules/reports/config/reports.client.config.js
+++ public/modules/reports/config/reports.client.config.js
@@ -4,7 +4,7 @@
 angular.module('reports').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Reports', 'reports', 'dropdown', '/reports(/create)?', false, ['admin','producer/auditions director','talent director'],2);
+		Menus.addMenuItem('topbar', 'Reports', 'reports', 'dropdown', '/reports(/create)?', false, ['admin','producer/auditions director','talent director'], 2);
 		//Menus.addSubMenuItem('topbar', 'reports', 'Generate Reports', 'reports');
 		Menus.addSubMenuItem('topbar', 'reports', 'Missing Auditions', 'reports/missing-auditions');
 		Menus.addSubMenuItem('topbar', 'reports', 'Auditions Booked', 'reports/auditions-booked');
