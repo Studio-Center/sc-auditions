68a9f562b10c9d4f5636908d65f1a77fa23a9092
rob <rob@studiocenter.com>
2015-07-14 17:32:54 -0400

    talent filter tweak

--- public/modules/core/filters/core.multiTermFull.client.filter.js
+++ public/modules/core/filters/core.multiTermFull.client.filter.js
@@ -3,7 +3,6 @@ angular.module('core').filter('multiTermFull', function($filter){
      return function(inputArray, searchText){
         var wordArray = searchText ? searchText.toLowerCase().split(/\s+/) : [];
         var wordCount = wordArray.length;
-        console.log(inputArray);
         for(var i=0;i<wordCount;i++){
             inputArray = $filter('filter')(inputArray, wordArray[i], false);
         }
