91c83caa83049c865c498f673d0124be65d4fc34
rob <rob@studiocenter.com>
2015-07-14 17:30:20 -0400

    talent filter options

--- public/modules/core/filters/core.multiTermFull.client.filter.js
+++ public/modules/core/filters/core.multiTermFull.client.filter.js
@@ -1,10 +1,11 @@
 'use strict';
-angular.module('core').filter('multiTerm', function($filter){
+angular.module('core').filter('multiTermFull', function($filter){
      return function(inputArray, searchText){
         var wordArray = searchText ? searchText.toLowerCase().split(/\s+/) : [];
         var wordCount = wordArray.length;
+        console.log(inputArray);
         for(var i=0;i<wordCount;i++){
-            inputArray = $filter('filter')(inputArray, wordArray[i], true);
+            inputArray = $filter('filter')(inputArray, wordArray[i], false);
         }
         return inputArray;
     };
