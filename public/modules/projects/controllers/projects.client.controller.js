5ae26ca04ca0cae74457aa47ae3fe3f6bd821bf5
rob <rob@studiocenter.com>
2015-07-15 10:47:49 -0400

    prod coord rules

--- public/modules/projects/controllers/projects.client.controller.js
+++ public/modules/projects/controllers/projects.client.controller.js
@@ -282,6 +282,17 @@ angular.module('projects').controller('ProjectsController', ['$scope', '$statePa
 				}
 			}
 		};
+		$scope.permitAdminProductionCoordinatorDirector = function(){
+			var allowRoles = ['admin', 'producer/auditions director', 'production coordinator'];
+
+			for(var i = 0; i < Authentication.user.roles.length; ++i){
+				for(var j = 0; j < allowRoles.length; ++j){
+					if(Authentication.user.roles[i] === allowRoles[j]) {
+						return true;
+					}
+				}
+			}
+		};
 		$scope.permitAdminDirector = function(){
 			var allowRoles = ['admin', 'producer/auditions director'];
 
