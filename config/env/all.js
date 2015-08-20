fa081366c4eed17a546aae21e7adda271088c698
rob <rob@studiocenter.com>
2015-08-20 10:04:23 -0400

    basic settings and 404

--- config/env/all.js
+++ config/env/all.js
@@ -3,8 +3,8 @@
 module.exports = {
 	app: {
 		title: 'Studio Center Auditions',
-		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-		keywords: 'MongoDB, Express, AngularJS, Node.js'
+		description: 'Studio Center website application for audition project management.',
+		keywords: 'studio center, auditions, project management, audio projects'
 	},
 	port: process.env.PORT || 3000,
 	templateEngine: 'swig',
--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
+		state('error', {
+			url: '/404',
+			templateUrl: 'modules/core/views/not-found.client.view.html'
 		});
 	}
 ]);
