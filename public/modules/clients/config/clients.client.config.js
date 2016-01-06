99d0ebf41519006fa18ab379494d7eb2828b8aef
Robert McDowell <doodersrage@gmail.com>
2016-01-06 14:03:52 -0500

    more menu tweaks

--- public/modules/clients/config/clients.client.config.js
+++ public/modules/clients/config/clients.client.config.js
@@ -6,5 +6,6 @@ angular.module('clients').run(['Menus', 'Authentication',
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Clients', 'clients', 'dropdown', '/clients/projects', false, ['admin', 'producer/auditions director', 'production coordinator', 'talent director', 'client', 'client-client'], 1);
 		Menus.addSubMenuItem('topbar', 'clients', 'Portal', 'clients/projects', false);
-	}
+		Menus.addSubMenuItem('topbar', 'clients', 'Start A New Audition Project', 'clients/new-audition-form', false);
+}
 ]);
