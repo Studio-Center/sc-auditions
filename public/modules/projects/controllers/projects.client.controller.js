52a573cfc19a525735da10501804d2710e863679
Robert McDowell <doodersrage@gmail.com>
2016-04-21 11:05:35 -0400

    fix user id check error

--- public/modules/projects/controllers/projects.client.controller.js
+++ public/modules/projects/controllers/projects.client.controller.js
@@ -352,7 +352,7 @@ angular.module('projects').controller('ProjectsController', ['$scope', '$statePa
 			var project = $scope.project,
 					authUsr = Authentication.user;
 
-			if(String(project.owner) === String(authUsr._id) || String(project.user._id) === String(authUsr._id)){
+			if((String(project.owner) === String(authUsr._id) && typeof project.user === 'object') || String(project.user._id) === String(authUsr._id)){
 				return true;
 			} else {
 				return false;
