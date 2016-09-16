f7c79664113ca7c13d50d2015c448a56a727a3e5
Robert McDowell <doodersrage@gmail.com>
2016-09-16 15:44:24 -0400

    always redirect client to portal on home page

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,10 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		if(typeof $scope.authentication.user !== 'undefined' && ($scope.authentication.user.roles[0] === 'client' || $scope.authentication.user.roles[0] === 'client-client')) {
+			$location.path('/clients/projects');
+		}
+
 	}
 ]);
