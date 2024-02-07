163f40734fd5b9914376534793bdea68cb45efc5
Robert McDowell <doodersrage@gmail.com>
2024-02-07 11:41:46 -0500

    socketio

--- public/modules/core/services/socket.js
+++ public/modules/core/services/socket.js
@@ -4,9 +4,10 @@
 var io;
 angular.module('core').factory('Socket', ['socketFactory',
     function(socketFactory) {
+        console.log(location.host);
         return socketFactory({
             prefix: '',
-            ioSocket: io($location.protocol()+"://"+$location.host()+(80!==$location.port()||443!==$location.port()?":"+$location.port():""), {reconnect: true, 'transports': ['websocket', 'polling']})
+            ioSocket: io(location.host, {reconnect: true, 'transports': ['websocket', 'polling']})
         });
     }
 ]);
