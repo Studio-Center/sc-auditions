2315adbe2140109f67518ca7cf7c45b3a7529e31
Robert McDowell <doodersrage@gmail.com>
2024-03-14 09:40:45 -0400

    Fix top nav sort order

--- public/modules/talents/config/talents.client.config.js
+++ public/modules/talents/config/talents.client.config.js
@@ -4,7 +4,7 @@
 angular.module('talents').run(['Menus', 'Authentication',
 	function(Menus, Authentication) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Talent', 'talents', 'dropdown', '/talents(/create)?', false, ['admin', 'producer/auditions director','auditions director','audio intern', 'production coordinator', 'talent director'], 3);
+		Menus.addMenuItem('topbar', 'Talent', 'talents', 'dropdown', '/talents(/create)?', false, ['admin', 'producer/auditions director','auditions director','audio intern', 'production coordinator', 'talent director'], 4);
 		Menus.addSubMenuItem('topbar', 'talents', 'Browse', 'talents', false, false, ['admin','producer/auditions director','auditions director','audio intern', 'production coordinator', 'talent director']);
 		Menus.addSubMenuItem('topbar', 'talents', 'New', 'talents/create', false, false, ['admin', 'talent director','producer/auditions director','auditions director','audio intern', 'production coordinator']);
 	}
