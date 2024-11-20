e29a0fc3f727053c203a3421f475ea49f5fa9fab
Robert McDowell <doodersrage@gmail.com>
2024-11-20 09:59:22 -0500

    error output mod

--- app/controllers/errors.server.controller.js
+++ app/controllers/errors.server.controller.js
@@ -24,34 +24,19 @@ var getUniqueErrorMessage = function(err) {
  */
 exports.getErrorMessage = function(err) {
 	let message = '',
-	logString = '';
+		logString = '';
 
 	if(typeof err === 'string'){
 		message = err;
 	} else {
-		// if (err.code) {
-		// 	switch (err.code) {
-		// 		case 11000:
-		// 		case 11001:
-		// 			message = getUniqueErrorMessage(err);
-		// 			break;
-		// 		default:
-		// 			message = 'Something went wrong';
-		// 	}
-		// } else {
-		// 	for (let errName in err.errors) {
-		// 		if (err.errors[errName].message) message = err.errors[errName].message;
-		// 	}
-		// }
 		message = err;
 	}
 
 	logString = moment().format('MM-DD-YYYY hh:mm a') + ' ' + message;
-	message.logString = logString;
 
 	// write message to console
 	console.log(logString);
 
 	// return value
-	return message;
+	return logString;
 };
